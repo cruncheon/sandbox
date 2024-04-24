16beb23520373118914f8d59fef14e63584a8e21
cruncheon <38970996+cruncheon@users.noreply.github.com>
2024-04-24 16:43:40 +0900

    Disable taskbar annoyances
    Disables:
    - Search bar
    - Task view
    - News feeds

--- BurpSandboxConfig.ps1
+++ BurpSandboxConfig.ps1
@@ -1,2 +1,14 @@
+# Disable Search Bar
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search -Name 'SearchBoxTaskbarMode' -Value 0 -Type 'DWord' -Force
+
+# Disable Task View
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name 'ShowTaskViewButton' -Type 'DWord' -Value 0 
+
+# Disable News
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Feeds -Name 'ShellFeedsTaskbarViewMode' -Value 2 -Type 'DWord' -Force
+
+# Restart Explorer
+Get-Process explorer | Stop-Process -force  
+
 # Install Burp Suite
 Start-Job -FilePath c:\sandbox\InstallBurp.ps1
